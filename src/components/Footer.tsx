49a21bfc650f75329e34339c432c8ce71dc452aa
Sam.G <samuelgerungan@gmail.com>
2024-03-04 19:37:58 -0800

    Update Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer-container">
+      <ul className="footer-menu">
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
